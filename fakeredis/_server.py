6e0751992b0fe4f14f5bd024e05e3266654ce257
Daniel M <cunla@users.noreply.github.com>
2022-05-14 05:44:05 -0400

    Fix python3.8 redis4.2+ issue (#6)
    Add support for redis-py4.3.1.
    Remove python3.7.
    
    Fix issues #3, #5

--- fakeredis/_server.py
+++ fakeredis/_server.py
@@ -3,7 +3,6 @@ import hashlib
 import inspect
 import itertools
 import logging
-import math
 import pickle
 import queue
 import random
@@ -15,6 +14,7 @@ import weakref
 from collections import defaultdict
 from collections.abc import MutableMapping
 
+import math
 import redis
 import six
 
@@ -87,7 +87,7 @@ RESTORE_INVALID_TTL_MSG = "ERR Invalid TTL value, must be >= 0"
 FLAG_NO_SCRIPT = 's'  # Command not allowed in scripts
 
 # This needs to be grabbed early to avoid breaking tests that mock redis.Redis.
-_ORIG_SIG = inspect.signature(redis.Redis)
+# _ORIG_SIG = inspect.signature(redis.Redis)
 
 
 class SimpleString:
@@ -2799,20 +2799,23 @@ class FakeRedisMixin:
     def __init__(self, *args, server=None, connected=True, **kwargs):
         # Interpret the positional and keyword arguments according to the
         # version of redis in use.
-        bound = _ORIG_SIG.bind(*args, **kwargs)
-        bound.apply_defaults()
-        if not bound.arguments['connection_pool']:
-            charset = bound.arguments['charset']
-            errors = bound.arguments['errors']
+        default_args = inspect.signature(redis.Redis.__init__).parameters.values()
+        kwds = {p.name: p.default for p in default_args if p.default != inspect._empty}
+        kwds.update(kwargs)
+        # bound = _ORIG_SIG.bind(*args, **params)
+        # kwds = bound.arguments['kwds']
+        if not kwds['connection_pool']:
+            charset = kwds['charset']
+            errors = kwds['errors']
             # Adapted from redis-py
             if charset is not None:
                 warnings.warn(DeprecationWarning(
                     '"charset" is deprecated. Use "encoding" instead'))
-                bound.arguments['encoding'] = charset
+                kwds['encoding'] = charset
             if errors is not None:
                 warnings.warn(DeprecationWarning(
                     '"errors" is deprecated. Use "encoding_errors" instead'))
-                bound.arguments['encoding_errors'] = errors
+                kwds['encoding_errors'] = errors
 
             if server is None:
                 server = FakeServer()
@@ -2835,10 +2838,10 @@ class FakeRedisMixin:
                 'client_name'
             ]
             for arg in conn_pool_args:
-                if arg in bound.arguments:
-                    kwargs[arg] = bound.arguments[arg]
-            bound.arguments['connection_pool'] = redis.connection.ConnectionPool(**kwargs)
-        super().__init__(*bound.args, **bound.kwargs)
+                if arg in kwds:
+                    kwargs[arg] = kwds[arg]
+            kwds['connection_pool'] = redis.connection.ConnectionPool(**kwargs)
+        super().__init__(*args, **kwds)
 
     @classmethod
     def from_url(cls, *args, **kwargs):
