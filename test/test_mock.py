7f6b66d6ae3c77b6fa673cde933b608854b1fffd
Daniel M <daniel.maruani@gmail.com>
2022-07-24 09:11:43 -0400

    Refactor test files

--- test/test_mock.py
+++ test/test_mock.py
@@ -1,13 +1,14 @@
-from redis import Redis
+import redis
 
 
-def connect_redis(redis_host: str, redis_port: int) -> Redis:
-    redis_con = Redis(redis_host, redis_port)
+def connect_redis_conn(redis_host: str, redis_port: int) -> redis.Redis:
+    redis_con = redis.Redis(redis_host, redis_port)
     return redis_con
 
 
-def bar(zip, zap):
-    redis_con = connect_redis('localhost', 6379)
+def bar():
+    redis_con = connect_redis_conn('localhost', 6000)
+    pass
 
 
 from unittest.mock import patch
@@ -16,6 +17,8 @@ from fakeredis import FakeRedis
 
 def test_bar():
     # Mock Redis connection
-    with patch('test_mock.Redis', FakeRedis):
+    with patch('redis.Redis', FakeRedis):
         # Call function
-        bar(zip, [])
+        bar()
+
+        # Related to #36 - this should fail if mocking Redis does not work
