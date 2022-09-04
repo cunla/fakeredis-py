26a1fbd3327fa865ff2de15222b05e93bdbf9e64
Daniel M <daniel.maruani@gmail.com>
2022-09-04 09:16:06 -0400

    Implementing expire new params

--- test/test_fakeredis7.py
+++ test/test_fakeredis7.py
@@ -1,10 +1,16 @@
+from time import sleep
+from xmlrpc.client import ResponseError
+
 import pytest as pytest
 import redis
 
 from testtools import raw_command, zadd
 
+pytestmark = [
+    pytest.mark.min_server('7'),
+]
 
-@pytest.mark.min_server('7')
+
 def test_script_exists(r):
     # test response for no arguments by bypassing the py-redis command
     # as it requires at least one argument
@@ -26,14 +32,30 @@ def test_script_exists(r):
     assert r.script_exists("a", sha1_one, "c", sha1_two, "e", "f") == [0, 1, 0, 1, 0, 0]
 
 
-@pytest.mark.min_server('7')
 def test_set_get_nx(r):
     # Note: this will most likely fail on a 7.0 server, based on the docs for SET
     assert raw_command(r, 'set', 'foo', 'bar', 'NX', 'GET') is None
 
 
-@pytest.mark.min_server('7.0')
 def test_zadd_minus_zero(r):
     zadd(r, 'foo', {'a': -0.0})
     zadd(r, 'foo', {'a': 0.0})
     assert raw_command(r, 'zscore', 'foo', 'a') == b'0'
+
+
+@pytest.mark.slow
+def test_expire_should_expire_key(r):
+    r.set('foo', 'bar')
+    assert r.get('foo') == b'bar'
+    r.expire('foo', 1)
+    sleep(1.5)
+    assert r.get('foo') is None
+    assert r.expire('bar', 1) is False
+
+
+@pytest.mark.slow
+def test_expire_should_throw_error(r):
+    # r.set('foo', 'bar')
+    # assert r.get('foo') == b'bar'
+    with pytest.raises(ResponseError):
+        r.expire('foo', 1, nx=True, xx=True)
