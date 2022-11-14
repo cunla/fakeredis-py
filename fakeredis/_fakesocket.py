a1af7768f412653f50c6f33c3d53225d0c9b45b4
Mark S <mark.schlachter@technologyadvice.com>
2022-11-14 00:17:21 -0500

    chore: Add type annotations

--- fakeredis/_fakesocket.py
+++ fakeredis/_fakesocket.py
@@ -1,5 +1,19 @@
+"""FakeRedis's internal emulation of a Redis server socket."""
+
+# Future Imports
+from __future__ import annotations
+
+# Standard Library Imports
+from typing import (
+    TYPE_CHECKING,
+    ForwardRef,
+    Type,
+)
+
+# Third-Party Imports
 import redis
 
+# Imports From Package Sub-Modules
 from ._basefakesocket import BaseFakeSocket
 from .commands_mixins.bitmap_mixin import BitmapCommandsMixin
 from .commands_mixins.connection_mixin import ConnectionCommandsMixin
@@ -13,6 +27,12 @@ from .commands_mixins.set_mixin import SetCommandsMixin
 from .commands_mixins.sortedset_mixin import SortedSetCommandsMixin
 from .commands_mixins.string_mixin import StringCommandsMixin
 from .commands_mixins.transactions_mixin import TransactionsCommandsMixin
+
+if TYPE_CHECKING:
+    # Package-Level Imports
+    from fakeredis import FakeServer
+else:
+    FakeServer = ForwardRef("fakeredis.FakeServer")
 
 
 class FakeSocket(
@@ -30,7 +50,9 @@ class FakeSocket(
     BitmapCommandsMixin,
     SortedSetCommandsMixin,
 ):
-    _connection_error_class = redis.ConnectionError
+    """An emulated Redis server socket."""
 
-    def __init__(self, server):
+    _connection_error_class: Type[Exception] = redis.ConnectionError
+
+    def __init__(self, server: FakeServer) -> None:
         super(FakeSocket, self).__init__(server)
