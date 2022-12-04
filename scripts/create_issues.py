f1d13269662c44be38af702edc45e403413dcd74
Daniel M <daniel.maruani@gmail.com>
2022-12-03 20:42:07 -0500

    Implement support for ignored groups and commands

--- scripts/create_issues.py
+++ scripts/create_issues.py
@@ -4,12 +4,32 @@ Script to create issue for every unsupported command.
 
 from supported import get_unimplemented_and_implemented_commands, download_redis_commands
 
+IGNORE_GROUPS = {
+    'server', 'cf', 'cms', 'topk', 'tdigest', 'bf', 'search', 'suggestion', 'timeseries',
+    'graph', 'server', 'cluster', 'connection',
+}
+IGNORE_COMMANDS = {
+    'PUBSUB HELP',
+    'OBJECT HELP',
+    'FUNCTION HELP',
+    'SCRIPT HELP',
+    'XGROUP HELP',
+    'XINFO HELP',
+    'JSON.DEBUG HELP',
+    'JSON.DEBUG MEMORY',
+    'JSON.DEBUG',
+}
+
 
 def print_gh_commands(commands: dict, unimplemented: dict):
     for group in unimplemented:
+        if group in IGNORE_GROUPS:
+            continue
         print(f'### Creating issues for {group} commands')
         for item in unimplemented[group]:
             cmd = item.upper()
+            if cmd in IGNORE_COMMANDS:
+                continue
             link = f"https://redis.io/commands/{item.replace(' ', '-')}/"
             summary = commands[item]['summary']
             title = f"Implement support for \`{cmd}\` ({group} command)"
