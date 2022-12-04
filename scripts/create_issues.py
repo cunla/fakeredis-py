68e0eb3194226aa4a5422e650ba800471777bbd8
Daniel M <daniel.maruani@gmail.com>
2022-12-04 18:59:10 -0500

    Create issues from script

--- scripts/create_issues.py
+++ scripts/create_issues.py
@@ -1,12 +1,17 @@
 """
 Script to create issue for every unsupported command.
 """
+import os
 
 from supported import get_unimplemented_and_implemented_commands, download_redis_commands
+from github import Github
 
 IGNORE_GROUPS = {
     'server', 'cf', 'cms', 'topk', 'tdigest', 'bf', 'search', 'suggestion', 'timeseries',
     'graph', 'server', 'cluster', 'connection',
+    'server', 'cluster', 'list', 'connection', 'bitmap', 'sorted-set', 'generic', 'scripting', 'geo', 'string', 'hash',
+    'hyperloglog', 'pubsub', 'stream', 'json', 'graph', 'timeseries', 'search', 'suggestion', 'bf', 'cf', 'cms', 'topk',
+    'tdigest'
 }
 IGNORE_COMMANDS = {
     'PUBSUB HELP',
@@ -21,23 +26,53 @@ IGNORE_COMMANDS = {
 }
 
 
+class GithubData:
+    def __init__(self, dry=True):
+        token = os.getenv('GITHUB_TOKEN', None)
+        g = Github(token)
+        self.dry = dry or (token is None)
+        self.gh_repo = g.get_repo('cunla/fakeredis')
+        open_issues = self.gh_repo.get_issues(state='open')
+        self.issues = {i.title: i.number for i in open_issues}
+        gh_labels = self.gh_repo.get_labels()
+        self.labels = {label.name for label in gh_labels}
+
+    def create_label(self, name):
+        if self.dry:
+            print(f'Creating label "{name}"')
+        else:
+            self.gh_repo.create_label(name, "f29513")
+
+    def create_issue(self, group: str, cmd: str, summary: str):
+        link = f"https://redis.io/commands/{cmd.replace(' ', '-')}/"
+        title = f"Implement support for `{cmd.upper()}` ({group} command)"
+        filename = f'{group}_mixin.py'
+        body = f"""Implement support for command `{cmd.upper()}` in {filename}
+        {summary}. 
+        [Full documentation]({link}))"""
+        labels = [f'{group}-commands', 'enhancement', 'help wanted']
+        for label in labels:
+            if label not in self.labels:
+                self.create_label(label)
+        if title in self.issues:
+            return
+        if self.dry:
+            print(f'Creating issue with title "{title}" and labels {labels}')
+        else:
+            self.gh_repo.create_issue(title, body, labels=labels)
+
+
 def print_gh_commands(commands: dict, unimplemented: dict):
+    gh = GithubData()
     for group in unimplemented:
         if group in IGNORE_GROUPS:
             continue
         print(f'### Creating issues for {group} commands')
-        for item in unimplemented[group]:
-            cmd = item.upper()
-            if cmd in IGNORE_COMMANDS:
+        for cmd in unimplemented[group]:
+            if cmd.upper() in IGNORE_COMMANDS:
                 continue
-            link = f"https://redis.io/commands/{item.replace(' ', '-')}/"
-            summary = commands[item]['summary']
-            title = f"Implement support for \`{cmd}\` ({group} command)"
-            filename = f'{group}_mixin.py'
-            body = f"""Implement support for command \`{cmd}\` in {filename}
-{summary}. 
-[Full documentation]({link}))"""
-            print(f'gh issue create --title "{title}" --body "{body}" --label "enhancement,help wanted"')
+            summary = commands[cmd]['summary']
+            gh.create_issue(group, cmd, summary)
 
 
 if __name__ == '__main__':
