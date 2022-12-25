274efbd8f2864ffc38929c4a57ab857404e5cfb1
Daniel M <daniel.maruani@gmail.com>
2022-12-25 13:23:41 -0500

    Implement support for `BITPOS` (bitmap command)

--- fakeredis/_msgs.py
+++ fakeredis/_msgs.py
@@ -55,6 +55,8 @@ JSON_KEY_NOT_FOUND = "ERR could not perform this operation on a key that doesn't
 JSON_PATH_NOT_FOUND_OR_NOT_STRING = "ERR Path '{}' does not exist or not a string"
 JSON_PATH_DOES_NOT_EXIST = "ERR Path '{}' does not exist"
 LCS_CANT_HAVE_BOTH_LEN_AND_IDX = "ERR If you want both the length and indexes, please just use IDX."
+BIT_ARG_MUST_BE_ZERO_OR_ONE = "ERR The bit argument must be 1 or 0."
+
 FLAG_NO_SCRIPT = 's'  # Command not allowed in scripts
 FLAG_LEAVE_EMPTY_VAL = 'v'
 FLAG_TRANSACTION = 't'
