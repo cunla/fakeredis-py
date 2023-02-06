45d175014a2cc3d9be3eef59a306bbb39973d332
Daniel M <daniel.maruani@gmail.com>
2023-02-06 11:01:04 -0800

    Fix import error

--- fakeredis/stack/__init__.py
+++ fakeredis/stack/__init__.py
@@ -3,7 +3,7 @@ try:
     from redis.commands.json.path import Path  # noqa: F401
     from ._json_mixin import JSONCommandsMixin, JSONObject  # noqa: F401
 except ImportError as e:
-    if e.name != 'jsonpath_ng':
+    if e.name == 'fakeredis.stack._json_mixin':
         raise e
 
     class JSONCommandsMixin:
