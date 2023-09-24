4df5a4fc729ace872bd09bc91b61fcf7086eeb83
--get <daniel@moransoftware.ca>
2023-09-24 14:10:03 -0400

    feat(bf):bf.exists

--- fakeredis/stack/_bf_mixin.py
+++ fakeredis/stack/_bf_mixin.py
@@ -17,6 +17,9 @@ class BFCommandsMixin:
         key.updated()
         return 0 if res else 1
 
+    def _bf_exist(key: CommandItem, item: bytes) -> int:
+        return 1 if (item in key.value) else 0
+
     @command(
         name="BF.ADD",
         fixed=(Key(BloomFilter), bytes),
@@ -43,3 +46,22 @@ class BFCommandsMixin:
     )
     def bf_card(self, key):
         return len(key.value)
+
+    @command(
+        name="BF.EXISTS",
+        fixed=(Key(BloomFilter), bytes),
+        repeat=(),
+    )
+    def bf_exist(self, key, value: bytes):
+        return BFCommandsMixin._bf_exist(key, value)
+
+    @command(
+        name="BF.MEXISTS",
+        fixed=(Key(BloomFilter), bytes),
+        repeat=(bytes,),
+    )
+    def bf_mexists(self, key, *values: bytes):
+        res = list()
+        for value in values:
+            res.append(BFCommandsMixin._bf_exist(key, value))
+        return res
--- test/test_stack/test_bloomfilter.py
+++ test/test_stack/test_bloomfilter.py
@@ -21,10 +21,32 @@ def test_bf_madd(r: redis.Redis):
 
 
 def test_bf_card(r: redis.Redis):
-    assert r.bf().madd('key', 'v1', 'v2', 'v2') == [1, 1, 0]
-    assert r.bf().card('key') == 2
+    assert r.bf().madd('key', 'v1', 'v2', 'v3') == [1, 1, 1]
+    assert r.bf().card('key') == 3
     assert r.bf().card('key-new') == 0
 
     r.set('key1', 'value')
     with pytest.raises(redis.exceptions.ResponseError):
         r.bf().card('key1')
+
+
+def test_bf_exists(r: redis.Redis):
+    assert r.bf().madd('key', 'v1', 'v2', 'v3') == [1, 1, 1]
+    assert r.bf().exists('key', 'v1') == 1
+    assert r.bf().exists('key', 'v5') == 0
+    assert r.bf().exists('key-new', 'v5') == 0
+
+    r.set('key1', 'value')
+    with pytest.raises(redis.exceptions.ResponseError):
+        r.bf().add('key1', 'v')
+
+
+def test_bf_mexists(r: redis.Redis):
+    assert r.bf().madd('key', 'v1', 'v2', 'v3') == [1, 1, 1]
+    assert r.bf().mexists('key', 'v1') == [1, ]
+    assert r.bf().mexists('key', 'v1', 'v5') == [1, 0]
+    assert r.bf().mexists('key-new', 'v5') == [0, ]
+
+    r.set('key1', 'value')
+    with pytest.raises(redis.exceptions.ResponseError):
+        r.bf().add('key1', 'v')
