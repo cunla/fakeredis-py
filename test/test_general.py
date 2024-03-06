19e097dd97632201686a220ca4e03cbcc9ad7cb4
Daniel M <daniel.maruani@gmail.com>
2024-03-06 18:15:53 -0500

    fix:remove failing test

--- test/test_general.py
+++ test/test_general.py
@@ -13,14 +13,3 @@ def test_asyncioio_is_used():
 def test_unknown_command(r: redis.Redis):
     with pytest.raises(redis.ResponseError):
         raw_command(r, '0 3 3')
-
-
-def test_new_server_when_no_params():
-    from fakeredis import FakeRedis
-
-    fake_redis_1 = FakeRedis()
-    fake_redis_2 = FakeRedis()
-
-    fake_redis_1.set("foo", "bar")
-
-    assert fake_redis_2.get("foo") is None
