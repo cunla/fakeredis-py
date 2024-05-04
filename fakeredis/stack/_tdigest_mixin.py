a38ada2c72b97795fdefd4a9aa2f24f4441b8c13
Daniel M <daniel.maruani@gmail.com>
2024-05-04 11:07:36 -0400

    implemented create, reset, add

--- fakeredis/stack/_tdigest_mixin.py
+++ fakeredis/stack/_tdigest_mixin.py
@@ -5,7 +5,7 @@ from sortedcontainers import SortedList
 from fakeredis import _msgs as msgs
 from fakeredis._command_args_parsing import extract_args
 from fakeredis._commands import command, CommandItem, Int, Key, Float
-from fakeredis._helpers import SimpleString, SimpleError, OK
+from fakeredis._helpers import SimpleString, SimpleError, OK, Database
 
 
 class TDigest(SortedList):
@@ -15,6 +15,8 @@ class TDigest(SortedList):
 
 
 class TDigestCommandsMixin:
+    def __init__(self, *args, **kwargs):
+        self._db: Database
 
     @command(name="TDIGEST.CREATE", fixed=(Key(TDigest),), repeat=(bytes,),
              flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
@@ -64,6 +66,50 @@ class TDigestCommandsMixin:
         raise NotImplementedError
 
     @command(
+        name="TDIGEST.MERGE", fixed=(Key(TDigest), Int, bytes), repeat=(bytes,),
+        flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
+    def tdigest_merge(self, dest: CommandItem, numkeys: int, *args: bytes) -> SimpleString:
+        if len(args) < numkeys:
+            raise SimpleError(msgs.WRONG_ARGS_MSG6.format("tdigest.merge"))
+        sources_names = args[:numkeys]
+        (compression, override), _ = extract_args(args[numkeys:], ("+compression", "override"))
+        sources = [self._db.get(name).value for name in sources_names if name in self._db]
+        if len(sources) != len(sources_names):
+            raise SimpleError(msgs.TDIGEST_KEY_NOT_EXISTS)
+
+        if override:
+            if dest.value is None:
+                compression = compression or max([source.compression for source in sources])
+                dest.value = TDigest(compression)
+            else:
+                dest.value.clear()
+        if dest.value is None:
+            raise SimpleError(msgs.TDIGEST_KEY_NOT_EXISTS)
+        for source in sources:
+            dest.value.update(source)
+        dest.updated()
+        return OK
+
+    @command(
+        name="TDIGEST.MAX", fixed=(Key(TDigest),), repeat=(),
+        flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
+    def tdigest_max(self, key: CommandItem) -> bytes:
+        if key.value is None:
+            raise SimpleError(msgs.TDIGEST_KEY_NOT_EXISTS)
+        if len(key.value) == 0:
+            return b"nan"
+        return str(key.value[-1]).encode()
+
+    @command(name="TDIGEST.MIN", fixed=(Key(TDigest),), repeat=(),
+             flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
+    def tdigest_min(self, key: CommandItem) -> bytes:
+        if key.value is None:
+            raise SimpleError(msgs.TDIGEST_KEY_NOT_EXISTS)
+        if len(key.value) == 0:
+            return b"nan"
+        return str(key.value[0]).encode()
+
+    @command(
         name="TDIGEST.CDF", fixed=(Key(TDigest), Float), repeat=(Float,),
         flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
     def tdigest_cdf(self, key: CommandItem, *values: float) -> List[bytes]:
@@ -77,30 +123,13 @@ class TDigestCommandsMixin:
             b"Compression", key.value.compression,
             b"Capacity", len(key.value),
             b"Merged nodes", len(key.value),
-            b"Unmerged nodes", len(key.value),
+            b"Unmerged nodes", 0,
             b"Merged weight", len(key.value),
-            b"Unmerged weight", len(key.value),
+            b"Unmerged weight", 0,
             b"Observations", len(key.value),
             b"Total compressions", len(key.value),
             b"Memory usage", len(key.value),
         ]
-
-    @command(
-        name="TDIGEST.MAX", fixed=(Key(TDigest),), repeat=(),
-        flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
-    def tdigest_max(self, key: CommandItem) -> List[bytes]:
-        raise NotImplementedError
-
-    @command(
-        name="TDIGEST.MERGE", fixed=(Key(TDigest), Int, Key(TDigest)), repeat=(Key(TDigest),),
-        flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
-    def tdigest_merge(self, key: CommandItem, numkeys: int, values: CommandItem) -> SimpleString:
-        raise NotImplementedError
-
-    @command(name="TDIGEST.MIN", fixed=(Key(TDigest),), repeat=(),
-             flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
-    def tdigest_min(self, key: CommandItem) -> List[bytes]:
-        raise NotImplementedError
 
     @command(name="TDIGEST.QUANTILE", fixed=(Key(TDigest), Float), repeat=(Float,),
              flags=msgs.FLAG_DO_NOT_CREATE + msgs.FLAG_LEAVE_EMPTY_VAL)
