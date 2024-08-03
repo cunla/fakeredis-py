2677e718b94818f1b41fb8e2d07e3e84bf9600b1
Daniel M <cunla@users.noreply.github.com>
2024-08-03 15:38:02 -0400

    fix:bug with responses written twice in async redis connection (#317)

--- fakeredis/commands_mixins/connection_mixin.py
+++ fakeredis/commands_mixins/connection_mixin.py
@@ -2,7 +2,7 @@ from typing import Any, List, Union
 
 from fakeredis import _msgs as msgs
 from fakeredis._commands import command, DbIndex
-from fakeredis._helpers import SimpleError, OK, SimpleString, Database
+from fakeredis._helpers import SimpleError, OK, SimpleString, Database, casematch
 
 PONG = SimpleString(b"PONG")
 
@@ -36,4 +36,10 @@ class ConnectionCommandsMixin:
     def select(self, index: DbIndex) -> SimpleString:
         self._db = self._server.dbs[index]
         self._db_num = index  # type: ignore
+        return OK
+
+    @command(name="CLIENT SETINFO", fixed=(bytes, bytes), repeat=())
+    def client_setinfo(self, lib_data: bytes, value: bytes) -> SimpleString:
+        if not casematch(lib_data, b"LIB-NAME") and not casematch(lib_data, b"LIB-VER"):
+            raise SimpleError(msgs.SYNTAX_ERROR_MSG)
         return OK
