327a99c5ac566de62e7f4cdf109c5960f6c7ab31
Daniel M <daniel.maruani@gmail.com>
2025-05-03 10:08:21 -0400

    add client-list basic support

--- test/test_mixins/test_connection.py
+++ test/test_mixins/test_connection.py
@@ -53,7 +53,7 @@ def test_hello(r: redis.Redis):
 @pytest.mark.min_server("7")
 def test_client_list(r: redis.Redis):
     client_info = r.client_info()
-    client_list = r.client_list(client_id=[client_info["id"], client_info["id"] + 1])
+    client_list = r.client_list(client_id=[str(client_info["id"]), str(client_info["id"] + 1)])
     assert isinstance(client_list, list)
     assert len(client_list) >= 1
     assert isinstance(client_list[0], dict)
@@ -64,7 +64,7 @@ def test_client_list(r: redis.Redis):
     assert isinstance(client_list, list)
     client_ids = [int(client["id"]) for client in client_list]
     non_existing_client_id = max(client_ids) + 1
-    client_list = r.client_list(client_id=[non_existing_client_id])
+    client_list = r.client_list(client_id=[str(non_existing_client_id)])
     assert isinstance(client_list, list)
     assert len(client_list) == 0
 
