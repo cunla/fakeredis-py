31591adcd6e7fe94be2de12d1970de0d2aa1593d
Oliver Haas <ohaas@e1plus.de>
2025-10-28 22:13:05 +0100

    Fix TcpFakeServer EVALSHA NOSCRIPT error handling
    - Add NOSCRIPT prefix to NoScriptError messages in TCP protocol
    - Add test for EVALSHA with missing script

---
+++ test/test_tcp_server/test_scripting.py
@@ -0,0 +1,27 @@
+import time
+from threading import Thread
+
+import pytest
+import redis
+
+from fakeredis import TcpFakeServer
+from test import testtools
+
+
+@testtools.run_test_if_lupa_installed()
+def test_evalsha_missing_script():
+    """Test that EVALSHA with a non-existent script returns NOSCRIPT error."""
+    server_address = ("127.0.0.1", 19000)
+    server = TcpFakeServer(server_address)
+    t = Thread(target=server.serve_forever, daemon=True)
+    t.start()
+    time.sleep(0.1)
+
+    with redis.Redis(host=server_address[0], port=server_address[1]) as r:
+        fake_sha = "0" * 40
+        with pytest.raises(redis.exceptions.NoScriptError):
+            r.evalsha(fake_sha, 0)
+
+    server.server_close()
+    server.shutdown()
+    t.join()
