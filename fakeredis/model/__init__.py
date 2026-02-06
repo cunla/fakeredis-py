cfe601ee54ceb45ddeaf208f4c3036a47e2651a7
Daniel M <daniel.maruani@gmail.com>
2026-02-06 10:27:49 -0300

    feat:type command for different models

--- fakeredis/model/__init__.py
+++ fakeredis/model/__init__.py
@@ -1,14 +1,13 @@
 from ._acl import AccessControlList
 from ._base_type import BaseModel
 from ._client_info import ClientInfo
-from ._cms import CountMinSketch
+
 from ._command_info import (
     get_all_commands_info,
     get_command_info,
     get_categories,
     get_commands_by_category,
 )
-from ._cuckoo_filter import ScalableCuckooFilter
 from ._expiring_members_set import ExpiringMembersSet
 from ._hash import Hash
 from ._stream import XStream, StreamEntryKey, StreamGroup, StreamRangeTest
@@ -16,6 +15,19 @@ from ._tdigest import TDigest
 from ._timeseries_model import TimeSeries, TimeSeriesRule, AGGREGATORS
 from ._topk import HeavyKeeper
 from ._zset import ZSet
+
+try:
+    import probables  # noqa: F401
+    from ._cuckoo_filter import ScalableCuckooFilter
+    from ._cms import CountMinSketch
+except ImportError:
+
+    class CountMinSketch:
+        pass
+
+    class ScalableCuckooFilter:
+        pass
+
 
 __all__ = [
     "CountMinSketch",
