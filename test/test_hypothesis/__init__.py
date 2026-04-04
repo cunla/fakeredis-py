293191b41996fceb27320281c605d348d617bf02
Daniel M <daniel.maruani@gmail.com>
2026-04-04 19:01:29 -0400

    wip

--- test/test_hypothesis/__init__.py
+++ test/test_hypothesis/__init__.py
@@ -8,13 +8,15 @@ __all__ = [
     "TestTransaction",
     "TestZSet",
     "TestZSetNoScores",
+    "TestJoint",
 ]
 
-from test.test_hypothesis.test_connection import TestConnection
-from test.test_hypothesis.test_hash import TestHash
-from test.test_hypothesis.test_list import TestList
-from test.test_hypothesis.test_server import TestServer
-from test.test_hypothesis.test_set import TestSet
-from test.test_hypothesis.test_string import TestString
-from test.test_hypothesis.test_transaction import TestTransaction
-from test.test_hypothesis.test_zset import TestZSet, TestZSetNoScores
+from .test_connection import TestConnection
+from .test_hash import TestHash
+from .test_list import TestList
+from .test_server import TestServer
+from .test_set import TestSet
+from .test_string import TestString
+from .test_transaction import TestTransaction
+from .test_zset import TestZSet, TestZSetNoScores
+from .test_hypothesis_joint import TestJoint
