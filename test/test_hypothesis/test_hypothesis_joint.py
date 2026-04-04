26c2848023711e1f7d4ccb173ff0ba1c9fbfe4ad
Daniel M <daniel.maruani@gmail.com>
2026-04-04 19:06:49 -0400

    wip

--- test/test_hypothesis/test_hypothesis_joint.py
+++ test/test_hypothesis/test_hypothesis_joint.py
@@ -1,13 +1,13 @@
 import hypothesis.strategies as st
 
-from .test_connection import TestConnection
-from .test_server import TestServer
-from .test_zset import TestZSet
-from .test_set import TestSet
-from .test_list import TestList
-from .test_hash import TestHash
+from .test_connection import TestConnection as _TestConnection
+from .test_server import TestServer as _TestServer
+from .test_zset import TestZSet as _TestZSet
+from .test_set import TestSet as _TestSet
+from .test_list import TestList as _TestList
+from .test_hash import TestHash as _TestHash
 from .base import BaseTest, common_commands, commands
-from .test_string import TestString, string_commands
+from .test_string import TestString as _TestString, string_commands
 
 bad_commands = (
     # redis-py splits the command on spaces, and hangs if that ends up being an empty list
@@ -17,20 +17,20 @@ bad_commands = (
 
 class TestJoint(BaseTest):
     create_command_strategy = (
-        TestString.create_command_strategy
-        | TestHash.create_command_strategy
-        | TestList.create_command_strategy
-        | TestSet.create_command_strategy
-        | TestZSet.create_command_strategy
+        _TestString.create_command_strategy
+        | _TestHash.create_command_strategy
+        | _TestList.create_command_strategy
+        | _TestSet.create_command_strategy
+        | _TestZSet.create_command_strategy
     )
     command_strategy = (
-        TestServer.server_commands
-        | TestConnection.connection_commands
+        _TestServer.server_commands
+        | _TestConnection.connection_commands
         | string_commands
-        | TestHash.hash_commands
-        | TestList.list_commands
-        | TestSet.set_commands
-        | TestZSet.zset_commands
+        | _TestHash.hash_commands
+        | _TestList.list_commands
+        | _TestSet.set_commands
+        | _TestZSet.zset_commands
         | common_commands
         | bad_commands
     )
