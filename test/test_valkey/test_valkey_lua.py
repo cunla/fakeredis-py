661a0d66aaa3b1421387fc0e28b5b1ff78592234
Przemysław Buczkowski <prem@prem.moe>
2026-05-09 10:33:28 +0100

    Use r fixture

--- test/test_valkey/test_valkey_lua.py
+++ test/test_valkey/test_valkey_lua.py
@@ -1,25 +1,27 @@
 import pytest
 
 import fakeredis
+import redis
+import valkey
 
 _ = pytest.importorskip("lupa")
 
 
-def test_server_call():
-    r = fakeredis.FakeStrictValkey(server_type="valkey")
+@pytest.mark.unsupported_server_types("redis")
+def test_server_call(r: valkey.Valkey):
     r.set("key", "value")
     result = r.eval("return server.call('GET', KEYS[1])", 1, "key")
     assert result == b"value"
 
 
-def test_server_pcall():
-    r = fakeredis.FakeStrictValkey(server_type="valkey")
+@pytest.mark.unsupported_server_types("redis")
+def test_server_pcall(r: valkey.Valkey):
     r.set("key", "value")
     result = r.eval("return server.pcall('GET', KEYS[1])", 1, "key")
     assert result == b"value"
 
 
-def test_server_alias_not_available_in_redis_mode():
-    r = fakeredis.FakeStrictRedis()
+@pytest.mark.unsupported_server_types("valkey")
+def test_server_alias_not_available_in_redis_mode(r: redis.Redis):
     with pytest.raises(Exception, match="server"):
         r.eval("return server.call('SET', KEYS[1], ARGV[1])", 1, "key", "value")
