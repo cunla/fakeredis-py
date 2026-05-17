feace90ba451c97a9455e9dc04537c7e01aad8bb
Przemysław Buczkowski <prem@prem.moe>
2026-05-17 18:16:49 +0100

    Fix r fixture typing

--- test/test_valkey/test_valkey_lua.py
+++ test/test_valkey/test_valkey_lua.py
@@ -1,19 +1,18 @@
 import pytest
 
-import redis
-import valkey
+from fakeredis._typing import ClientType
 
 _ = pytest.importorskip("lupa")
 
 
 @pytest.mark.unsupported_server_types("redis", "dragonfly")
-def test_server_is_alias_for_redis(r: valkey.Valkey):
+def test_server_is_alias_for_redis(r: ClientType):
     result = r.eval("return tostring(server == redis)", 0)
     assert result == b"true"
 
 
 @pytest.mark.unsupported_server_types("valkey")
-def test_server_alias_not_available_in_redis_mode(r: redis.Redis):
+def test_server_alias_not_available_in_redis_mode(r: ClientType):
     # Check if server exists in globals
     result = r.eval("return tostring(rawget(_G, 'server') == nil)", 0)
     assert result == b"true"
