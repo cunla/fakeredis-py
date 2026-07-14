2829f1c78b5c94d0d6cc9842f02b70c2d03e12d2
Daniel M <daniel.maruani@gmail.com>
2026-07-14 16:19:00 -0400

    wip

--- test/test_mixins/test_specific_redis_versions/test_redis_8_8_zset_count.py
+++ test/test_mixins/test_specific_redis_versions/test_redis_8_8_zset_count.py
@@ -8,7 +8,7 @@ pytestmark.extend(
     [
         pytest.mark.supported_server_versions(min_redis_ver="8.7.2"),
         pytest.mark.unsupported_server_types("dragonfly", "valkey"),
-        testtools.run_test_if_redispy_ver("gt", "7.0.0"),
+        testtools.run_test_if_redispy_ver("gte", "7.0.0"),
     ]
 )
 
